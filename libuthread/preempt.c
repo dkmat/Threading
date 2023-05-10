f56ece76a8bff836f2034f56016862f729cf4e15
dkmat <dmathur@ucdavis.edu>
2023-05-09 20:05:56 -0700

    modified:   libuthread/preempt.c

--- libuthread/preempt.c
+++ libuthread/preempt.c
@@ -13,7 +13,14 @@
  * Frequency of preemption
  * 100Hz is 100 times per second
  */
+#define _XOPEN_SOURCE 700
 #define HZ 100
+
+void sig_handler(int signum){
+	(void)signum;
+	uthread_yield();
+}
+
 
 void preempt_disable(void)
 {
@@ -28,12 +35,21 @@ void preempt_enable(void)
 void preempt_start(bool preempt)
 {
 	if(preempt){
-		//sigaction();
+		struct sigaction sig;
+		struct sigaction old_sig;
+		sig.sa_handler = sig_handler;
+		sigaction(SIGVTALRM,&sig,&old_sig);
+		struct itimerval timer;
+		timer.it_interval.tv_sec = 0;
+		timer.it_interval.tv_usec = 10;
+		timer.it_value = timer.it_interval;
+		setitimer(ITIMER_VIRTUAL,&timer,NULL);
 	}
 }
 
 void preempt_stop(void)
 {
-	/* TODO Phase 4 */
+	sigaction(SIGVTALRM,NULL,NULL);
+	setitimer(ITIMER_VIRTUAL,NULL,NULL);
 }
 
